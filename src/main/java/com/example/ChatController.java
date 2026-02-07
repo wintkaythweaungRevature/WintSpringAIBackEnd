3e04cd70c9d37a2277e35730c7749cdc632b6138
Wint Kay <ms.wintkaythweaung@gmail.com>
2026-02-06 20:05:39 -0500

    chat

--- src/main/java/com/example/ChatController.java
+++ src/main/java/com/example/ChatController.java
@@ -1,9 +1,8 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ai.openai.OpenAiChatModel;
+
 
 @RestController
 public class ChatController {
