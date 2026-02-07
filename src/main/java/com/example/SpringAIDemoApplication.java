8a5ffaafe7e6dbca0be7ad6292e5fb6815d7f854
Wint Kay <ms.wintkaythweaung@gmail.com>
2026-02-06 19:25:44 -0500

    Fixed: Added component scan to main application

--- src/main/java/com/example/SpringAIDemoApplication.java
+++ src/main/java/com/example/SpringAIDemoApplication.java
@@ -2,8 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example")
 public class SpringAIDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringAIDemoApplication.class, args);
