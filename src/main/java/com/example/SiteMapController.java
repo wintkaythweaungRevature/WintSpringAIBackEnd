1cc2c7ce0facba37ee1fc4dcb830fe9d4a9500c1
Wint Kay <ms.wintkaythweaung@gmail.com>
2026-03-04 15:19:41 -0500

    d

--- src/main/java/com/example/SiteMapController.java
+++ src/main/java/com/example/SiteMapController.java
@@ -1,7 +1,5 @@
 package com.example;
 
-public package com.example;
-
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -12,12 +10,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SiteMapController {
 
-    // ဤနေရာတွင် /api/ai လုံးဝ မပါရပါ။
     @GetMapping(value = "/sitemap.xml", produces = MediaType.APPLICATION_XML_VALUE)
-    @ResponseBody
-    public Resource getSitemap() {
-        return new ClassPathResource("static/sitemap.xml");
-    }
-} {
-    
+@ResponseBody
+public String getSitemap() {
+    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+           "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n" +
+           "  <url>\n" +
+           "    <loc>https://wintaibot.com/</loc>\n" +
+           "    <lastmod>2026-03-04</lastmod>\n" +
+           "    <priority>1.0</priority>\n" +
+           "  </url>\n" +
+           "</urlset>";
 }
+} 
